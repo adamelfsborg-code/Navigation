05bc84760f53216d11076ab5dd452c6f6135980b
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-23 16:58:58 +0200

    feat: toolbar item placement

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -3,13 +3,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<100) {i in
-                    Text("row \(i)")
-            }
-            .navigationTitle("Title goes here")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.indigo)
-            .toolbarColorScheme(.dark)
+            Text("Hello, there")
+                .toolbar {
+                    ToolbarItemGroup(placement: .confirmationAction) {
+                        Button("Tap me") {}
+                        Button("Or me"){}
+                    }
+                }
         }
     }
 }
