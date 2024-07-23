55e766004704a8594792e95301a9eb177e7a501d
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-23 16:52:34 +0200

    feat: style toolbar

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -1,63 +1,15 @@
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.applicationDirectory.appending(path: "SavedPath")
-    
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-       path = NavigationPath()
-    }
-    
-    func save() {
-        guard let representation = path.codable else {return }
-        do {
-            let data = try? JSONEncoder().encode(representation)
-            try data?.write(to: savePath)
-        } catch {
-           print("Failed to save data.")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    @Binding var path: NavigationPath
-    
-    var body: some View {
-        VStack {
-            
-            NavigationLink("You selected number: \(number)", value: Int.random(in: 0...1000))
-                .navigationTitle("Num - \(number)")
-                .toolbar {
-                    Button("Reset") {
-                        path = NavigationPath()
-                    }
-                }
-        }
-        
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { selection in
-                    DetailView(number: selection, path: $pathStore.path)
-                }
+        NavigationStack {
+            List(0..<100) {i in
+                    Text("row \(i)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.indigo)
+            .toolbarColorScheme(.dark)
         }
     }
 }
