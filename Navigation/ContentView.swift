487ae825e5f94d8627c95bdbc3161ca391aa52a7
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-23 16:01:35 +0200

    feat: navigation destination

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -1,14 +1,40 @@
 import SwiftUI
 
-struct ContentView: View {
+struct DetailView: View {
+    let person: Person
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("\(person.name)")
+    }
+    
+    init(person: Person) {
+        print("Creating detail view for: \(person.id)")
+        self.person = person
+    }
+}
+
+struct Person: Hashable, Identifiable {
+    let id = UUID()
+    var name: String
+}
+
+struct ContentView: View {
+    @State var persons = [Person]()
+    var body: some View {
+        NavigationStack {
+            List(persons) { person in
+                NavigationLink("Tap row \(person.name)", value: person)
+            }
+            .navigationDestination(for: Person.self) { selection in
+                DetailView(person: selection)
+            }
+            .toolbar {
+                Button("Add person") {
+                    let p = Person(name: "Adam \(Int.random(in: 1..<10))")
+                    persons.insert(p, at: 0 )
+                }
+            }
         }
-        .padding()
     }
 }
 
